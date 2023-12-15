b23622b5771809c0a79cfb8ffc50d9b507ab0242
JacobCEU <124430062+JacobCEU@users.noreply.github.com>
2023-12-15 18:34:02 +0800

    frontend fixes

---
+++ frontend/js/adminLogout.js
@@ -0,0 +1,7 @@
+function logoutUser() {
+    // Implement your logout logic here
+    // For example, you might want to clear session/local storage or make an API call to log out the user.
+    // After that, redirect the user to the login page or the desired destination.
+    console.log("User logged out"); // Placeholder message
+    window.location.href = '../html/adminLogin.html';
+}
