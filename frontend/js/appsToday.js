d128f7f323caa250666dc3124e3593643deea246
JacobCEU <124430062+JacobCEU@users.noreply.github.com>
2023-12-18 01:39:08 +0800

    Functional admin

--- frontend/js/appsToday.js
+++ frontend/js/appsToday.js
@@ -1,71 +1,35 @@
-let url = `http://localhost:8000/admin/viewToday`;
-fetchInfo(url);
+// appsToday.js
 
+// Function to format time
 function formatTime(rawTime) {
   const timeParts = rawTime.split(':');
   return `${(+timeParts[0] % 12) || 12}:${timeParts[1]} ${(timeParts[0] >= 12) ? 'PM' : 'AM'}`;
 }
 
-function api_client(url, content, callback) {
-  console.log('URL:', url);
-  console.log('Content:', content);
-
-  fetch(url, content)
-    .then(response => {
-      console.log('Response status:', response.status);
-      return response.json();
-    })
+// Function to fetch and display today's appointments
+function fetchTodayAppointments() {
+  let url = 'http://localhost:8000/admin/viewToday';
+  fetch(url)
+    .then(response => response.json())
     .then(data => {
-      console.log('Data from server:', data);
-      const namesRow = document.getElementById("namesRows");
-      const timeRow = document.getElementById("timeRows");
-      const serviceRow = document.getElementById("serviceRows");
-      const statusRow = document.getElementById("statusRows");
+      const appointmentsTableBody = document.getElementById('appointmentsTableBody');
+      appointmentsTableBody.innerHTML = ''; // Clear previous content
 
-      namesRow.innerHTML = ""; // Clear previous content
-      timeRow.innerHTML = "";
-      serviceRow.innerHTML = "";
-      statusRow.innerHTML = "";
-
-      const appointments = data.data;
-
-      console.log(appointments);
-
-      if (appointments && appointments.length > 0) {
-        appointments.forEach(appointment => {
-          // Populate Name row
-          namesRow.innerHTML += `
-            <div class="name">
-                <label>${appointment.firstName} ${appointment.lastName}</label>
-            </div>
-          `;
-
-          // Populate Time row
-          timeRow.innerHTML += `
-            <div class="time">
-                <label for="${appointment.time}">${formatTime(appointment.time)}</label>
-            </div>
-          `;
-
-          // Populate Service row
-          serviceRow.innerHTML += `
-            <div class="service">
-                <label for="${appointment.service.toLowerCase().replace(/\s+/g, '-')}">${appointment.service}</label>
-            </div>
-          `;
-
-          // Populate Status row
-          statusRow.innerHTML += `
-            <div class="status">
-                <label for="${appointment.status.toLowerCase()}">${appointment.status}</label>
-            </div>
+      if (data.successful && data.appointments && data.appointments.length > 0) {
+        data.appointments.forEach(appointment => {
+          // Append a new row for each appointment
+          appointmentsTableBody.innerHTML += `
+            <tr>
+              <td>${appointment.firstName} ${appointment.lastName}</td>
+              <td>${formatTime(appointment.time)}</td>
+              <td>${appointment.service}</td>
+              <td>${appointment.status}</td>
+            </tr>
           `;
         });
       } else {
-        namesRow.textContent = "No appointments today";
-        timeRow.textContent = "";
-        serviceRow.textContent = "";
-        statusRow.textContent = "";
+        // Display a message if there are no appointments
+        appointmentsTableBody.innerHTML = '<tr><td colspan="4">No appointments available</td></tr>';
       }
     })
     .catch(error => {
@@ -73,19 +37,7 @@ function api_client(url, content, callback) {
     });
 }
 
-function fetchInfo(url) {
-  let content = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
-
-  api_client(url, content, (response) => {
-    if (response.successful === true) {
-      alert(response.message);
-    } else {
-      alert("No appointments today");
-    }
-  });
-}
+// Call the function when the DOM is loaded
+document.addEventListener('DOMContentLoaded', function () {
+  fetchTodayAppointments();
+});
